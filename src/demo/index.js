d47b0ccc296bde9dab74a6e481f883b576a3e52a
池圣齐 <chi.shengqi@cgtn.com>
2021-09-22 17:35:54 +0800

    增加高优先级抢占低优先级任务demo

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,20 +2,21 @@ import React, { Component, createElement } from 'react';
 import { render, createRoot } from 'react-dom';
 import FnComp from './FnComp'
 import ClassComp from './ClassComp'
+import HighestPriority from './HighestPriority'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ClassComp />
+                <HighestPriority />
             </div>
         );
     }
 }
 
-render(<App />, document.getElementById('root'));
+//render(<App />, document.getElementById('root'));
 
 
-// createRoot(
-//     document.getElementById('root')
-// ).render(<App />);
+createRoot(
+    document.getElementById('root')
+).render(<App />);
